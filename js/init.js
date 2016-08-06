1771d887bdea2214ed40c5e5a1817c677a4bfa5f
Vipin Khushu <vipinkhushu@hotmail.com>
2016-08-06 14:58:48 +0530

    inital template

---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+	$(function(){
+		$('.parallax').parallax();
+		$(".button-collapse").sideNav();
+		 $('.modal-trigger').leanModal();
+		 $('select').material_select();
+		  $('.carousel').carousel();
+		    $(".button-collapse").sideNav();
+		    $('.collapsible').collapsible();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+$(document).ready(function() {
+
+			
+});
